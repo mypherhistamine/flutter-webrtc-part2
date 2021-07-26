fec6edfd3165e7c62ae919695db59abcb75e42d9
Rishabh Mishra <rishabhmishra23599@gmail.com>
2021-07-26 17:32:16 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _getUserMedia() async {
     final Map<String, dynamic> constraints = {
-      'audio': false,
+      'audio': true,
       'video': {
         'facingMode': 'user',
       },
